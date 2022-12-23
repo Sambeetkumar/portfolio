50a96e625f9878a3f20922cc293c2bd79d1518fa
sambeet kumar khatua <khatuasambeet31@gmail.com>
2022-12-23 22:52:37 +0530

    added cursor animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
@@ -9,6 +8,12 @@ import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
 function App() {
+let cursor = document.querySelector(".cursor");
+let cursor2 = document.querySelector(".cursor2");
+document.addEventListener("mousemove", function (e) {
+  cursor.style.cssText = cursor2.style.cssText =
+    "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+});
   return (
     <div className="App">
       <NavBar />
@@ -17,6 +22,8 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <div className="cursor"></div>
+      <div className="cursor2"></div>
     </div>
   );
 }

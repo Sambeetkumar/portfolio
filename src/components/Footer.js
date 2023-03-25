118fdead07be577364614500f1cda725f112c5dd
sambeet kumar khatua <khatuasambeet31@gmail.com>
2023-03-26 02:53:38 +0530

    Mobile responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,14 +11,14 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <MailchimpForm />
-          <Col size={12} sm={6}>
+          <Col size={12} sm={6} className="footlogobox">
             <img src={logo} alt="footer-logo" className="footer-logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://in.linkedin.com/in/sambeet-kumar-khatua-186bb7214" target="_blank"><img src={navIcon1} alt="Icon" /></a>
-              <a href="https://en-gb.facebook.com/khatua.sambeet/" target="_blank"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://www.instagram.com/sambeet_03/" target="_blank"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://in.linkedin.com/in/sambeet-kumar-khatua-186bb7214" target="_blank" rel="noreferrer"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://en-gb.facebook.com/khatua.sambeet/" target="_blank" rel="noreferrer"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/sambeet_03/" target="_blank" rel="noreferrer"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
